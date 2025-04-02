c0fcfa4a06a17d27e5927f8e9992a535330103cc
JensKrijgsman <jens.krijgsman@howest.be>
2025-04-02 12:00:06 +0200

    fix(cors): add additional allowed origins for CORS configuration

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -2,6 +2,8 @@ import { defineEventHandler, setResponseHeader } from 'h3';
 
 export default defineEventHandler((event) => {
   setResponseHeader(event, 'Access-Control-Allow-Origin', 'https://aiupd8frontend.netlify.app');
+  setResponseHeader(event, 'Access-Control-Allow-Origin', 'https://aiupdate.be');
+  setResponseHeader(event, 'Access-Control-Allow-Origin', 'https://aiupdate.howest.be');
   setResponseHeader(event, 'Access-Control-Allow-Origin', 'https://localhost:3000');
   setResponseHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   setResponseHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization');
