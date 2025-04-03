651eaefd441aa16edf17f27fe6a9c7a28a88ed28
JensKrijgsman <jens.krijgsman@howest.be>
2025-04-03 19:48:42 +0200

    feat(filter-bar): restructure button layout for improved usability and mobile responsiveness

--- components/FilterBar.vue
+++ components/FilterBar.vue
@@ -49,42 +49,49 @@
       />
     </div>
     <div class="w-full sm:w-48 flex flex-col justify-end mt-[24px]">
-      <div class="flex gap-2 w-full justify-center sm:justify-start">
-        <Button
-          label="Search"
-          icon="pi pi-search"
-          @click="applyFilters"
-          class="flex-grow search-button"
-        />
-        <Button
-          label="More Filters"
-          icon="pi pi-filter"
-          @click="toggleFilters"
-          class="p-button-outlined more-filters-button"
-        />
-        <div class="compare-button-container">
+      <div class="flex flex-col sm:flex-row gap-2 w-full">
+        <!-- Search button in its own container -->
+        <div class="w-full mb-2 sm:mb-0">
           <Button
-            label="Compare"
-            class="p-button-outlined compare-button"
-            @click="openComparisonModal"
-          >
-            <span class="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 512"
-                class="w-4 h-4 mr-1"
-              >
-                <path
-                  fill="currentColor"
-                  d="M384 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L398.4 96c-5.2 25.8-22.9 47.1-46.4 57.3L352 448l160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0-192 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0 0-294.7c-23.5-10.3-41.2-31.6-46.4-57.3L128 96c-17.7 0-32-14.3-32-32s14.3-32 32-32l128 0c14.6-19.4 37.8-32 64-32s49.4 12.6 64 32zm55.6 288l144.9 0L512 195.8 439.6 320zM512 416c-62.9 0-115.2-34-126-78.9c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C627.2 382 574.9 416 512 416zM126.8 195.8L54.4 320l144.9 0L126.8 195.8zM.9 337.1c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C242 382 189.7 416 126.8 416S11.7 382 .9 337.1z"
-                />
-              </svg>
-              Compare
+            label="Search"
+            icon="pi pi-search"
+            @click="applyFilters"
+            class="w-full search-button"
+          />
+        </div>
+
+        <!-- Filter and Compare buttons in a separate container -->
+        <div class="flex gap-2 w-full justify-center sm:justify-start">
+          <Button
+            label="More Filters"
+            icon="pi pi-filter"
+            @click="toggleFilters"
+            class="p-button-outlined more-filters-button flex-grow sm:flex-grow-0"
+          />
+          <div class="compare-button-container flex-grow sm:flex-grow-0">
+            <Button
+              label="Compare"
+              class="p-button-outlined compare-button w-full"
+              @click="openComparisonModal"
+            >
+              <span class="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 512"
+                  class="w-4 h-4 mr-1"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M384 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L398.4 96c-5.2 25.8-22.9 47.1-46.4 57.3L352 448l160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0-192 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0 0-294.7c-23.5-10.3-41.2-31.6-46.4-57.3L128 96c-17.7 0-32-14.3-32-32s14.3-32 32-32l128 0c14.6-19.4 37.8-32 64-32s49.4 12.6 64 32zm55.6 288l144.9 0L512 195.8 439.6 320zM512 416c-62.9 0-115.2-34-126-78.9c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C627.2 382 574.9 416 512 416zM126.8 195.8L54.4 320l144.9 0L126.8 195.8zM.9 337.1c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1C242 382 189.7 416 126.8 416S11.7 382 .9 337.1z"
+                  />
+                </svg>
+                Compare
+              </span>
+            </Button>
+            <span v-if="comparisonCount > 0" class="comparison-badge">
+              {{ comparisonCount }}
             </span>
-          </Button>
-          <span v-if="comparisonCount > 0" class="comparison-badge">
-            {{ comparisonCount }}
-          </span>
+          </div>
         </div>
       </div>
     </div>
@@ -205,9 +212,17 @@ onMounted(() => {
 }
 
 .more-filters-button {
-  min-width: 140px;
+  min-width: 120px;
   text-align: center;
   white-space: nowrap;
+}
+
+/* Mobile adjustments */
+@media (max-width: 640px) {
+  .more-filters-button,
+  .compare-button {
+    width: 100%;
+  }
 }
 
 .compare-button-container {
